a5031b15b9fb8e6a12c1bea89aac5f668286c44a
Trent Spoolstra <tspoolst@gmail.com>
2025-05-19 00:16:11 -0600

    added screen size query

--- CrossPlatform1/Views/MainView.axaml.cs
+++ CrossPlatform1/Views/MainView.axaml.cs
@@ -1,3 +1,5 @@
+using System;
+using Avalonia;
 using Avalonia.Controls;
 
 namespace CrossPlatform1.Views;
@@ -7,5 +9,21 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+
+        // Initial update
+        UpdateScreenSizeLabel(this.Bounds);
+
+        // Subscribe to size changes
+        this.GetObservable(BoundsProperty)
+            .Subscribe(bounds => UpdateScreenSizeLabel(bounds));
+    }
+
+    private void UpdateScreenSizeLabel(Rect bounds)
+    {
+        if (this.FindControl<Label>("ScreenSizeLabel") is { } label)
+        {
+            var size = this.Bounds.Size;
+            label.Content = $"Window size: {size.Width:0} x {size.Height:0}";
+        }
     }
 }
